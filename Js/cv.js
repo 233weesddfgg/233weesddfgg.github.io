1943107921a088ef85ada0cc66382312ea4c7d74
tobias goncalves <totofacha2009@gmail.com>
2024-03-09 19:40:41 -0300

    primer git utilizado

---
+++ Js/cv.js
@@ -0,0 +1,4 @@
+
+document.getElementById("btn-1");addEventListener('click', function () {
+    console.log("hola, persona que a entrado a este proyecto, espero le sea de su agrado. y lo disfrute")
+})
